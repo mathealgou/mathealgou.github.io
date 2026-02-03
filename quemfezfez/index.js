f3de273db52862e332e1c7b5d59be9317b918128
mathealgou <mathealgou@gmail.com>
2026-02-03 11:09:47 -0300

    feat: new images

--- quemfezfez/index.js
+++ quemfezfez/index.js
@@ -1,6 +1,6 @@
 function getRandomImage() {
-	const MIN_NUMBER = 0;
-	const MAX_NUMBER = 10;
+	const MIN_NUMBER = 1;
+	const MAX_NUMBER = 30;
 	const randomNumber =
 		Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER + 1)) + MIN_NUMBER;
 
